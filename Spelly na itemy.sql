ef26fb3fbf291218f3fd08f1612624f18d5d9c41
Sparco6 <gomet.auto@centrum.cz>
2024-11-12 10:08:20 +0100

    Spell item equip
    all equip spells

---
+++ Spelly na itemy.sql
@@ -0,0 +1,244 @@
+**Bonus Healing s rozdílem Bonus Damage:**
+42125 - 21000 Bonus Damage + 10000 Bonus Healing
+43924 - 12000 Bonus Damage + 3100 Bonus Healing
+39874 - 6000 Bonus Damage + 2300 Bonus Healing
+42127 - 21000 Bonus Healing + 10000 Bonus Damage
+40232 - 12000 Bonus Healing + 3100 Bonus Damage
+33821 - 6000 Bonus Healing + 2300 Bonus Damage
+
+**Healing:**
+53200 - 25K Bonus Healing
+53201 - 50K Bonus Healing
+53202 - 75K Bonus Healing
+53203 - 100K Bonus Healing
+53204 - 125K Bonus Healing
+53205 - 150K Bonus Healing
+53282 - 300K Bonus Healing
+53283 - 600K Bonus Healing
+53284 - 1000K Bonus Healing
+53285 - 2000K Bonus Healing
+53286 - 4000K Bonus Healing
+53287 - 6000K Bonus Healing
+53288 - 8000K Bonus Healing
+53289 - 10000K Bonus Healing
+
+**Spell Powerell Power:**
+35844 - 959 Spell Power
+53206 - 5K Spell Power
+53207 - 10K Spell Power
+53208 - 15K Spell Power
+53209 - 20K Spell Power
+53210 - 25K Spell Power
+53211 - 30K Spell Power
+53212 - 35K Spell Power
+53213 - 40K Spell Power
+53214 - 45K Spell Power
+53215 - 50K Spell Power
+53216 - 55K Spell Power
+53217 - 60K Spell Power
+53218 - 65K Spell Power
+53219 - 70K Spell Power
+53220 - 75K Spell Power
+53221 - 80K Spell Power
+53222 - 85K Spell Power
+53223 - 90K Spell Power
+53224 - 95K Spell Power
+53225 - 100K Spell Power
+53226 - 500K Spell Power
+53227 - 1000K Spell Power
+53228 - 2000K Spell Power
+53229 - 4000K Spell Power
+
+**Normal Attack Power:**
+18060 - 200 Attack Power
+35866 - 880 Attack Power
+35785 - 980 Attack Power
+35868 - 1164 Attack Power
+35786 - 1296 Attack Power
+43925 - 2400 Attack Power
+44336 - 3600 Attack Power
+39807 - 6900 Attack Power
+35494 - 12800 Attack Power
+35868 - 21K Attack Power
+53170 - 5K Attack Power
+53171 - 10K Attack Power
+53172 - 15K Attack Power
+53173 - 20K Attack Power
+53174 - 25K Attack Power
+53175 - 30K Attack Power
+53176 - 35K Attack Power
+53177 - 40K Attack Power
+53178 - 45K Attack Power
+53179 - 50K Attack Power
+53180 - 55K Attack Power
+53181 - 60K Attack Power
+53182 - 65K Attack Power
+53183 - 70K Attack Power
+53184 - 75K Attack Power
+53185 - 80K Attack Power
+53186 - 85K Attack Power
+53187 - 90K Attack Power
+53188 - 95K Attack Power
+53189 - 100K Attack Power
+53190 - 200K Attack Power
+53191 - 400K Attack Power
+53192 - 800K Attack Power
+53193 - 1000K Attack Power
+53194 - 2000K Attack Power
+53195 - 4000K Attack Power
+53196 - 8000K Attack Power
+53197 - 10000K Attack Power
+53198 - 15000K Attack Power
+53199 - 20000K Attack Power
+
+**Feral Attack Power:**
+- 44900 - 321 Feral Attack Power
+- 44911 - 829 Feral Attack Power
+- 44917 - 1496 Feral Attack Power
+- 24994 - 3K Feral Attack Power
+- 28154 - 6K Feral Attack Power
+- 35876 - 32K Feral Attack Power
+- 35875 - 64K Feral Attack Power
+- 36072 - 96K Feral Attack Power
+- 35135 - 128K Feral Attack Power
+- 53120 - 10K Feral Attack Power
+- 53121 - 20K Feral Attack Power
+- 53122 - 30K Feral Attack Power
+- 53123 - 40K Feral Attack Power
+- 53124 - 50K Feral Attack Power
+- 53125 - 60K Feral Attack Power
+- 53126 - 70K Feral Attack Power
+- 53127 - 80K Feral Attack Power
+- 53128 - 90K Feral Attack Power
+- 53129 - 100K Feral Attack Power
+- 53130 - 110K Feral Attack Power
+- 53131 - 120K Feral Attack Power
+- 53132 - 130K Feral Attack Power
+- 53133 - 140K Feral Attack Power
+- 53134 - 150K Feral Attack Power
+- 53135 - 450K Feral Attack Power
+- 53136 - 1000K Feral Attack Power
+- 53137 - 2000K Feral Attack Power
+- 53138 - 4000K Feral Attack Power
+- 53139 - 8000K Feral Attack Power
+
+**% Spell Power from Intel:** :peepomage: 
+31837 - 7% Spell Power by int
+31838 - 14%  Spell Power by int
+31839 - 21%  Spell Power by int
+31840 - 28%  Spell Power by int
+31841 - 34%  Spell Power by int
+53140 - 40% Spell Power by int
+53141 - 45% Spell Power by int
+53142 - 50% Spell Power by int
+53143 - 55% Spell Power by int
+53144 - 60% Spell Power by int
+53145 - 65% Spell Power by int
+53146 - 70% Spell Power by int
+53147 - 75% Spell Power by int
+53148 - 80% Spell Power by int
+53149 - 85% Spell Power by int
+53150 - 90% Spell Power by int
+53151 - 95% Spell Power by int
+53152 - 100% Spell Power by int
+53153 - 105% Spell Power by int
+53154 - 110% Spell Power by int
+53155 - 115% Spell Power by int
+53156 - 120% Spell Power by int
+53157 - 125% Spell Power by int
+53158 - 130% Spell Power by int
+53159 - 135% Spell Power by int
+53160 - 140% Spell Power by int
+53161 - 145% Spell Power by int
+53162 - 150% Spell Power by int
+53163 - 300% Spell Power by int
+53164 - 450% Spell Power by int
+53165 - 600% Spell Power by int
+53166 - 800% Spell Power by int
+53167 - 1000% Spell Power by int
+53168 - 1500% Spell Power by int
+53169 - 2000% Spell Power by int
+
+**Ranged Attack Power:** :bow_and_arrow: 
+- 21459 - 5K Ranged Attack Power
+- 21460 - 10K Ranged Attack Power
+- 21461 - 15K Ranged Attack Power
+- 50120 - 20K Ranged Attack Power
+- 37976 - 25K Ranged Attack Power
+- 50121 - 30K Ranged Attack Power
+- 50122 - 35K Ranged Attack Power
+- 50123 - 40K Ranged Attack Power
+- 50124 - 45K Ranged Attack Power
+- 21462 - 50K Ranged Attack Power
+- 50125 - 55K Ranged Attack Power
+- 50126 - 60K Ranged Attack Power
+- 50127 - 65K Ranged Attack Power
+- 50128 - 70K Ranged Attack Power
+- 50129 - 75K Ranged Attack Power
+- 50130 - 80K Ranged Attack Power
+- 50131 - 85K Ranged Attack Power
+- 50132 - 90K Ranged Attack Power
+- 50133 - 95K Ranged Attack Power
+- 50134 - 100K Ranged Attack Power
+- 50135 - 105K Ranged Attack Power
+- 50136 - 110K Ranged Attack Power
+- 50137 - 115K Ranged Attack Power
+- 50138 - 120K Ranged Attack Power
+- 50139 - 200K Ranged Attack Power
+- 50140 - 400K Ranged Attack Power
+- 50141 - 800K Ranged Attack Power
+- 50142 - 1000K Ranged Attack Power
+- 50143 - 2000K Ranged Attack Power
+- 50144 - 4000K Ranged Attack Power
+- 50145 - 8000K Ranged Attack Power
+- 50146 - 10000K Ranged Attack Power
+
+**Attack Power %** :muscle: 
+- 53100 - Attack Power by 1%
+- 30902 - Attack Power by 2%
+- 53101 - Attack Power by 3%
+- 30903 - Attack Power by 4%
+- 53102 - Attack Power by 5%
+- 30904 - Attack Power by 6%
+- 53103 - Attack Power by 7%
+- 30905 - Attack Power by 8%
+- 53104 - Attack Power by 9%
+- 30906 - Attack Power by 10%
+- 53105 - Attack Power by 11%
+- 53106 - Attack Power by 12%
+- 53107 - Attack Power by 13%
+- 53108 - Attack Power by 14%
+- 53109 - Attack Power by 15%
+- 53110 - Attack Power by 20%
+- 53114 - Attack Power by 25%
+- 53111 - Attack Power by 30%
+- 53115 - Attack Power by 35%
+- 53112 - Attack Power by 40%
+- 53116 - Attack Power by 45%
+- 53113 - Attack Power by 50%
+- 53117 - Attack Power by 55%
+- 53118 - Attack Power by 60%
+- 53119 - Attack Power by 70%
+- 53086 - Attack Power by 80%
+- 53087- Attack Power by 90%
+- 53088 - Attack Power by 100%
+- 53089 - Attack Power by 200%
+
+**Pet % Damage** :guide_dog:
+53330 - 250% Pet Damage
+53331 - 500% Pet Damage
+53332 - 750% Pet Damage
+53333 - 1000% Pet Damage
+53334 - 2000% Pet Damage
+53335 - 4000% Pet Damage
+53336 - 8000% Pet Damage
+53337 - 10000% Pet Damage
+53338 - 20000% Pet Damage
+53339 - 30000% Pet Damage
+
+20262 - 2% strength
+20263 - 4% strength
+20264 - 6% strength
+20265 - 8% strength
+20266 - 10% strength
+43926 - 2400 Ignore armor
